d2dd35b7547c9a8d98e0b84f45df04029f5ef792
Nicola Ortolani <ortonico820@gmail.com>
2024-02-19 18:11:57 +0100

    Footer Sistemato

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,35 +1,35 @@
 <footer class="footer-distributed">
 
     <div class="container">
-        <div class="row">
-            <div class="col-12">
+        <div class="row justify-content-evenly">
+            <div class="col-12 col-lg-3 my-3 text-center">
                 <img class="img-footer" src="storage/app/public/images/logoAulabPost.png" alt="Logo the aulab post">
                 <p class="footer-links">
                     <a href="#" class="text-dark">Home</a>
                     <a href="#" class="text-dark">Blog</a>
                     <a href="#" class="text-dark">About</a>
                     <a href="#" class="text-dark">Faq</a>
-                    <a href="#" class="text-dark">Contattaci</a>
                 </p>
                 <p class="footer-company-name">The Aulab Post © 2024</p>
             </div>
 
-            <div class="col-12">
-                <div>
-                    <p>Contattati</p>
+            <div class="col-12 col-lg-3 my-3">
+                <div class="text-center">
+                    <p>Contattaci</p>
                     <p class="text-dark">+800 583 345</p>
                     <p><a href="mailto:support@theaulabpost.com"
                             class="text-dark email-footer">support@theaulabpost.com</a></p>
                 </div>
             </div>
 
-            <div class="col-12">
-                <p class="">
-                    <span>The Aulab Post</span>
+            <div class="col-12 col-md-4 col-lg-3 my-3 text-center">
+                <p>The Aulab Post</p>
+                <p>
                     "Esplora il mondo attraverso l'insaziabile sete di conoscenza. La cultura è la chiave che apre le
                     porte
                     della comprensione, la cultura è The Aulab Post."
                 </p>
+
                 <div class="footer-icons">
                     <p>Seguici su:</p>
                     <a href="#"><i class="fa fa-facebook"><i class="bi bi-facebook"></i></a>
