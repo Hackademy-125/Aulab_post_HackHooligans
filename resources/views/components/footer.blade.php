e008071167740e9275951742905cc2898913c083
Nicola Ortolani <ortonico820@gmail.com>
2024-02-19 15:37:22 +0100

    Refactoring del codice

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,58 +1,56 @@
 <footer class="footer-distributed top">
 
-    <div class="footer-left">
+  <div class="footer-left">
 
-      <img class="img" src="{{ asset('img/Minimal_initial_brand_logo_1.png') }}" alt="">
+    <img class="img" src="{{ asset('img/Minimal_initial_brand_logo_1.png') }}" alt="">
 
-      <p class="footer-links">
-        <a href="#" class="link-1">Home</a>
-        
-        <a href="#">Blog</a>
-      
-        <a href="#">About</a>
-        
-        <a href="#">Faq</a>
-        
-        <a href="#">Contattaci</a>
-      </p>
+    <p class="footer-links">
+      <a href="#" class="link-1">Home</a>
 
-      <p class="footer-company-name">The Aulab Post © 2024</p>
+      <a href="#">Blog</a>
+
+      <a href="#">About</a>
+
+      <a href="#">Faq</a>
+
+      <a href="#">Contattaci</a>
+    </p>
+
+    <p class="footer-company-name">The Aulab Post © 2024</p>
+  </div>
+
+  <div class="footer-center">
+
+    <p class="footer-company-about">
+      <span>Contattaci</span>
+    <div>
+      <i class="fa fa-phone"></i>
+      <p>+800 583 345</p>
     </div>
 
-    <div class="footer-center">
-
-        <p class="footer-company-about">
-            <span>Contattaci</span>
-      <div>
-        <i class="fa fa-phone"></i>
-        <p>+800 583 345</p>
-      </div>
-
-      <div>
-        <i class="fa fa-envelope"></i>
-        <p><a href="mailto:support@theaulabpost.com">support@theaulabpost.com</a></p>
-      </div>
-
+    <div>
+      <i class="fa fa-envelope"></i>
+      <p><a href="mailto:support@theaulabpost.com">support@theaulabpost.com</a></p>
     </div>
 
-    <div class="footer-right">
+  </div>
 
-      <p class="footer-company-about">
-        <span>About the company</span>
-        Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
-      </p>
+  <div class="footer-right">
 
-      <div class="footer-icons">
+    <p class="footer-company-about text-dark">
+      <span>About the company</span>
+      Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
+    </p>
 
-        <a href="#"><i class="fa fa-facebook"><i class="bi bi-facebook"></i></i></a>
-        <a href="#"><i class="fa fa-twitter"><i class="bi bi-whatsapp"></i></i></a>
-        <a href="#"><i class="fa fa-linkedin"><i class="bi bi-linkedin"></i>
-        </i></a>
-        <a href="#"><i class="fa fa-github"><i class="bi bi-instagram"></i>
-        </i></a>
+    <div class="footer-icons">
 
-      </div>
+      <a href="#"><i class="fa fa-facebook"><i class="bi bi-facebook"></i></a>
+      <a href="#"><i class="fa fa-twitter"><i class="bi bi-whatsapp"></i></a>
+      <a href="#"><i class="fa fa-linkedin"><i class="bi bi-linkedin"></i></a>
+      <a href="#"><i class="fa fa-github"><i class="bi bi-instagram"></i></a>
 
     </div>
 
-  </footer>
+  </div>
+
+</footer>
